1dfdfdf7bdc78c862b142400678c72b144f895b0
Kid Koder <codermann09@gmail.com>
2020-03-31 08:13:37 -0500

    More effect testing & named effect.allergies.allergic_reaction

--- src/main/java/net/kidkoder/allergies/init/EffectInit.java
+++ src/main/java/net/kidkoder/allergies/init/EffectInit.java
@@ -1,11 +1,6 @@
 package net.kidkoder.allergies.init;
 
-import net.kidkoder.allergies.object.item.ItemAllergyPill;
-import net.kidkoder.allergies.object.item.ItemAllergyShot;
-import net.kidkoder.allergies.object.item.ItemNormalInhaler;
-import net.kidkoder.allergies.object.item.ItemRescueInhaler;
 import net.kidkoder.allergies.potion.effect.EffectAllergicReaction;
-import net.minecraft.item.Item;
 import net.minecraft.potion.Effect;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
--- src/main/java/net/kidkoder/allergies/potion/effect/EffectAllergicReaction.java
+++ src/main/java/net/kidkoder/allergies/potion/effect/EffectAllergicReaction.java
@@ -1,13 +1,24 @@
 package net.kidkoder.allergies.potion.effect;
 
 import net.kidkoder.allergies.AllergiesMod;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
+
+import javax.annotation.Nullable;
 
 public class EffectAllergicReaction extends Effect {
     public EffectAllergicReaction() {
         super(EffectType.HARMFUL, 255);
         setRegistryName("allergic_reaction");
+    }
+
+    @Override
+    public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entity, int amplifier, double health) {
+
+        entity.setHealth(1);
+
+        super.affectEntity(source, indirectSource, entity, amplifier, health);
     }
 }
