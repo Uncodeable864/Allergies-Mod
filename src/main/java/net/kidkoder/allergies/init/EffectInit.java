bbd3b28e428d142e355e289461d8d2ce9e56c58a
Kid Koder <codermann09@gmail.com>
2020-03-31 17:41:00 -0500

    [TBT] Textured effects

--- src/main/java/net/kidkoder/allergies/init/EffectInit.java
+++ src/main/java/net/kidkoder/allergies/init/EffectInit.java
@@ -1,6 +1,7 @@
 package net.kidkoder.allergies.init;
 
 import net.kidkoder.allergies.effect.EffectAllergicReaction;
+import net.kidkoder.allergies.effect.EffectEpinephrine;
 import net.minecraft.potion.Effect;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,14 +11,15 @@ import net.minecraftforge.fml.common.Mod;
 public class EffectInit {
 
         public static Effect EFFECT_ALLERGIC_REACTION;
+        public static Effect EFFECT_EPINEPHRINE;
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Effect> event) {
 
         event.getRegistry().registerAll(
 
-                EFFECT_ALLERGIC_REACTION = new EffectAllergicReaction()
-
+                EFFECT_ALLERGIC_REACTION = new EffectAllergicReaction(),
+                EFFECT_EPINEPHRINE = new EffectEpinephrine()
         );
 
     }
