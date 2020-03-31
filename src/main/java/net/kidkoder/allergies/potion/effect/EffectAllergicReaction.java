e0b1d02b58214d635595a2ddba57a55c4c241fb7
Kid Koder <codermann09@gmail.com>
2020-03-31 12:02:53 -0500

    STUFF

--- src/main/java/net/kidkoder/allergies/potion/effect/EffectAllergicReaction.java
+++ src/main/java/net/kidkoder/allergies/potion/effect/EffectAllergicReaction.java
@@ -3,8 +3,10 @@ package net.kidkoder.allergies.potion.effect;
 import net.kidkoder.allergies.AllergiesMod;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Effects;
 
 import javax.annotation.Nullable;
 
@@ -12,13 +14,10 @@ public class EffectAllergicReaction extends Effect {
     public EffectAllergicReaction() {
         super(EffectType.HARMFUL, 255);
         setRegistryName("allergic_reaction");
-    }
+}
 
     @Override
-    public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entity, int amplifier, double health) {
-
-        entity.setHealth(1);
-
-        super.affectEntity(source, indirectSource, entity, amplifier, health);
+    public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entityLivingBaseIn, int amplifier, double health) {
+        super.affectEntity(source, indirectSource, entityLivingBaseIn, amplifier, health);
     }
 }
