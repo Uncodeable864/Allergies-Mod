eff2a8a75da1545ecaf2b7951215a8aa40ddf2f2
Kid Koder <codermann09@gmail.com>
2020-04-17 11:55:14 -0500

    [WIP]Edited attach capability

--- src/main/java/net/kidkoder/allergies/capability/CapabilityHandler.java
+++ src/main/java/net/kidkoder/allergies/capability/CapabilityHandler.java
@@ -9,16 +9,25 @@ package net.kidkoder.allergies.capability;
 
 import net.kidkoder.allergies.AllergiesMod;
 import net.kidkoder.allergies.capability.allergies.AllergiesProvider;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.FORGE)
 public class CapabilityHandler {
 
     public static final ResourceLocation ALLERGIES_LOC = new ResourceLocation(AllergiesMod.MODID, "allergies");
 
     @SubscribeEvent
-    public void attachCapability(AttachCapabilitiesEvent event) {
-        event.addCapability(ALLERGIES_LOC, new AllergiesProvider());
+    public void attachCapability(AttachCapabilitiesEvent<Entity> event) {
+
+        if(event.getObject() instanceof PlayerEntity) {
+            event.addCapability(ALLERGIES_LOC, new AllergiesProvider());
+        }
+
     }
+
 }
